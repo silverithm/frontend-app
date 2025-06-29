69479f914d44754abfd60778f6ea90f21259ca94
Kim Jun Hyeong <ggprgrkjh@naver.com>
2025-06-29 18:28:17 +0900

    앱 아이콘 및 패키지명 변경
    - 앱 아이콘을 carev_icon으로 교체 (모든 플랫폼)
    - Android 패키지명을 com.silverithm.carev로 변경
    - Firebase 프로젝트 설정 추가
    - 민감정보 파일들 .gitignore에 추가
    - 이전 패키지명 관련 파일들 정리

--- android/app/src/main/kotlin/com/silverithm/app/frontend_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.silverithm.app.frontend_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/silverithm/carev/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.silverithm.carev.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() 
